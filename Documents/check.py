1b43e67e47577eedacf5c60044a51d7eb059c32d
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-02-27 07:38:40 +0000

    27022023_2

--- Documents/check.py
+++ Documents/check.py
@@ -1,3 +1,5 @@
 import os
 liste = os.listdir("/workspace/MyoVeritabani2023_2/Exercises")
-print(len(liste))
+fileName = "ornek.sql"
+for item in liste:
+    open(f"/workspace/MyoVeritabani2023_2/Exercises/{item}/{fileName}","a+")
