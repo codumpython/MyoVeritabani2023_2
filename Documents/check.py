8398fe0db3f1d11694417aae32954bd3c34c9406
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-02-27 08:27:11 +0000

    27022023_4

--- Documents/check.py
+++ Documents/check.py
@@ -3,3 +3,22 @@ liste = os.listdir("/workspace/MyoVeritabani2023_2/Exercises")
 fileName = "ornek.sql"
 # for item in liste:
 #     open(f"/workspace/MyoVeritabani2023_2/Exercises/{item}/{fileName}","a+")
+
+for item in liste:
+    with open(f"/workspace/MyoVeritabani2023_2/Exercises/{item}/{fileName}","r") as dosya,\
+    open(f"/workspace/MyoVeritabani2023_2/Exercises/Cevaplar/{fileName}","r") as dosya2:
+        cevap = dosya2.read()
+        if dosya.read() != cevap:
+            print(item)
+
+# MUHAMMETYUSUFYILDIZ
+# BARANDEDE
+# GurayGULGUN
+# SEFADEMIRKAYA
+# HACİALİAKKOYUN
+# BERATKESKIN
+# KUTAYDEDE
+# BERATOZDEMIR
+# IREMCELIK
+# MIRACYELKEN
+# CERENKİLCİ
