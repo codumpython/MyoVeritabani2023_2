7d2649de6de6846416f93874810a4ce03a0d5ed3
İbrahim EDİZ <pythonegitimiediz@gmail.com>
2023-03-13 08:08:10 +0000

    13032023_4

--- Documents/check.py
+++ Documents/check.py
@@ -1,6 +1,6 @@
 import os
 liste = os.listdir("/workspace/MyoVeritabani2023_2/Exercises")
-fileName = "where3.sql"
+fileName = "where3_1.sql"
 for item in liste:
     open(f"/workspace/MyoVeritabani2023_2/Exercises/{item}/{fileName}","a+")
 
--- Exercises/ESRADURAK/where3.sql
+++ Exercises/ESRADURAK/where3.sql
@@ -1,2 +1,2 @@
--- SELECT Albumid,name FROM tracks WHERE trackid BETWEEN 20 AND 50;
+SELECT Albumid,name FROM tracks WHERE trackid BETWEEN 20 AND 50;
 SELECT Name,UnitPrice FROM Tracks WHERE TrackId<10;
